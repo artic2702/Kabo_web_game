9a7b55b9ca4837482b953a541ca61c20540207b8
artic2702 <mach20arnavag@gmail.com>
2026-04-01 02:43:56 +0530

    Fix Express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -195,7 +195,7 @@ import fs from 'fs';
 const distPath = path.join(__dirname, '..', 'dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
   console.log('[Server] Serving static frontend from dist/');
